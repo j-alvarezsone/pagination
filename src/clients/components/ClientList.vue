af97ca276b953b090e4a26f29cd7527c3113c1bd
Jorge Sone <jorge.sone@dataddo.com>
2022-11-26 18:57:34 +0100

    created client list component

---
+++ src/clients/components/ClientList.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <ul>
+    <RouterLink :to="{ name: 'client-id', params: { id: 1 } }">Client Name</RouterLink>
+  </ul>
+</template>
+
+<style scoped></style>
--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,8 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ClientList from '../components/ClientList.vue';
+</script>
 
 <template>
   <div>
     <h1>List of clients</h1>
+
+    <!-- Clients list -->
+    <ClientList />
+    <!-- Pagination numbers -->
+
+    <!-- Loading modal -->
   </div>
 </template>
 
